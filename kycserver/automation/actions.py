08ea4de72b47fbd5d0f92fad0f1db62f1f6a7c31
Laurkan <laurkanr@gmail.com>
2026-03-19 20:56:15 -0400

    added the blank=True to config so that actions without any parameters can be run

--- kycserver/automation/actions.py
+++ kycserver/automation/actions.py
@@ -1,24 +1,35 @@
 
 import logging
-from watchdog.models import Alert, SignalType
+from watchdog.models import Alert, Signal, SignalType
 from utils.action_runner import ActionRunner
 
 logger = logging.getLogger(__name__)
 
+def getSignalTypeIdFromSignalId(signal_id):
+    if signal_id == None:
+        return None
+    else:
+        try:
+            signal = Signal.objects.get(pk=signal_id)
+            return signal.signal_type
+        except Signal.DoesNotExist as e:
+            logger.error(f"Signal ID could not be found on a signal: {signal_id}")
+            return None
+        except Exception as e:
+            logger.error(f"Action Runner Exception: {e}")
+            return None
+
 @ActionRunner.register("create_alert")
 def create_alert_action(results, config, context):
     title = config.get("title", "Default Alert")
-    signal_type_id = config.get("signal_type_id")
-    logger.error(f"Title: {title}\n\tSignal Type: {signal_type_id}\n\n\tContext: {context}\n\n\tResults: {results}")
-    try:
-        signal_type_obj = SignalType.objects.get(pk=signal_type_id)
+
+    signal_type_obj = getSignalTypeIdFromSignalId(context.get("signal", None))
+    if signal_type_obj != None:
         alert_message = f"{title} - {signal_type_obj.label}"
         try:
             alert_obj = Alert.objects.create(message=alert_message, reason__pk=1, status__pk=1, severity__pk=1)
             alert_obj.save()
         except Exception as e:
             logger.error(f"Alert Object Issue: {e}")
-    except SignalType.DoesNotExist as e:
-        logger.error(f"Signal Type could not be found: ID -> {signal_type_id}\n\t{e}")
-    except Exception as e:
-        logger.error(f"Action Runner Exception: {e}")
+    else:
+        logger.error(f"Signal Type could not be found")
