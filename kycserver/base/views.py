c3946eb9298fea2ca9bd60745eeb254e2d9ccda0
Laurkan <laurkanr@gmail.com>
2026-03-24 20:28:39 -0400

    attempting to fix the can not find BaseViewSet error from Party.views

--- kycserver/base/views.py
+++ kycserver/base/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+
+class BaseViewSet(ModelViewSet):
+    permission_classes = [IsAuthenticated]
 
 # Create your views here.
