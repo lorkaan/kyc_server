103b0226e2147659eed4355e22bcd9cb43171213
Laurkan <laurkanr@gmail.com>
2026-05-20 22:59:01 -0400

    added the agenda event and agenda event type

--- kycserver/agenda/admin.py
+++ kycserver/agenda/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import AgendaEvent, AgendaEventType
+
 # Register your models here.
+admin.site.register(AgendaEventType)
+admin.site.register(AgendaEvent)
