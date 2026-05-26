ee969771f2ca8d65a609a8a670763fa00fe35ba9
Laurkan <laurkanr@gmail.com>
2026-05-26 12:28:13 -0400

    added the event status url endpoint

--- kycserver/agenda/urls.py
+++ kycserver/agenda/urls.py
@@ -1,11 +1,13 @@
 from rest_framework.routers import DefaultRouter
 from .views import (
     AgendaEventTypeViewSet,
-    AgendaEventViewSet
+    AgendaEventViewSet,
+    EventStatusListView
 )
 
 router = DefaultRouter()
 router.register(r"event-types", AgendaEventTypeViewSet)
 router.register(r"events", AgendaEventViewSet)
+router.register(r'event_status', EventStatusListView)
 
 urlpatterns = router.urls
