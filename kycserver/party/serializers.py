cf6a4a70b1f36eb729a9e14db92f2c2441073ddb
Laurkan <laurkanr@gmail.com>
2026-08-18 20:21:02 -0400

    removed the import of RoleRelationship

--- kycserver/party/serializers.py
+++ kycserver/party/serializers.py
@@ -1,6 +1,5 @@
 from django.apps import apps
 
-from kyc.serializers import RelationshipRoleSerializer
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 from django.db import transaction
